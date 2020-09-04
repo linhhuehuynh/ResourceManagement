92d0bb490d5b5d2683a3ec154aab8911327ac415
Linh Huynh <lhuynh5@students.towson.edu>
2020-09-04 13:29:16 -0400

    Project structure created

---
+++ ResourceManagement/src/main/java/com/groupproject/resourcemanagement/ResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.groupproject.resourcemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceManagementApplication.class, args);
+    }
+
+}
---
+++ ResourceManagement/src/test/java/com/groupproject/resourcemanagement/ResourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupproject.resourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
