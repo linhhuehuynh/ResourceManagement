452b58257e789c014741cccc263858dbcaab07bc
yuz203 <yuezh203@outlook.com>
2020-09-08 01:14:52 -0700

    add project and resourcr

---
+++ demoresourcemanagement/src/main/java/com/example/demoresourcemanagement/dao/ProjectDao.java
@@ -0,0 +1,10 @@
+package com.example.demoresourcemanagement.dao;
+
+import com.example.demoresourcemanagement.entity.Project;
+import com.example.demoresourcemanagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProjectDao extends JpaRepository<Project, Integer> {
+
+}
---
+++ demoresourcemanagement/src/main/java/com/example/demoresourcemanagement/dao/ResourceDao.java
@@ -0,0 +1,12 @@
+package com.example.demoresourcemanagement.dao;
+
+import com.example.demoresourcemanagement.entity.Project;
+import com.example.demoresourcemanagement.entity.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+
+public interface ResourceDao extends JpaRepository<Resource, Integer> {
+
+    Resource findByCreateDate(Date createDate);
+}
