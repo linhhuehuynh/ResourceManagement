37e3bfb9ab0bab301436105470d2ee6875bfd6de
jwang173 <jwang173@stevens.edu>
2020-09-10 00:05:54 +0800

    commit

---
+++ database/ss.sql
@@ -0,0 +1,8 @@
+INSERT INTO `sysRole` VALUES ('1', 'ROLE_ADMIN');
+INSERT INTO `sysRole` VALUES ('2', 'ROLE_USER');
+
+INSERT INTO `sysUser` VALUES ('1', 'admin', '123');
+INSERT INTO `sysUser` VALUES ('2', 'jitwxs', '123');
+
+INSERT INTO `sysUserRole` VALUES ('1', '1');
+INSERT INTO `sysUserRole` VALUES ('2', '2');
