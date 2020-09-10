bc3bea9d481ae335af5aff0e46193df229a12ee6
jwang173 <jwang173@stevens.edu>
2020-09-10 22:36:53 +0800

    security

--- SecurityDemo/src/main/java/com/example/SecurityDemo/security/CustomUserDetailService.java
+++ SecurityDemo/src/main/java/com/example/SecurityDemo/security/CustomUserDetailService.java
@@ -1,5 +1,14 @@
+package com.example.SecurityDemo.security;
+
 import com.example.SecurityDemo.entity.SysRole;
 import com.example.SecurityDemo.entity.SysUserRole;
+import com.example.SecurityDemo.service.SysRoleService;
+import com.example.SecurityDemo.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
