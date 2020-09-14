8f7b47c2b3decacf7bb573e79e8717f2a79169f3
Sean Qin <OseankingO@icloud.com>
2020-09-14 11:32:09 -0400

    added sucurity version

--- demoresourcemanagement/src/main/java/com/example/demoresourcemanagement/dao/UserCredentialDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoresourcemanagement.dao;
-
-import com.example.demoresourcemanagement.entity.UserCredential;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserCredentialDao extends JpaRepository<UserCredential, Integer> {
-}
