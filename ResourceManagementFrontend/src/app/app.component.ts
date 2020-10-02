7307a6d91a1d07178fcb970a6ff141a34dd70433
Linh Hue Huynh <linhhuehuynh0512@gmail.com>
2020-10-01 20:40:17 -0400

    updated app comp

--- ResourceManagementFrontend/src/app/app.component.ts
+++ ResourceManagementFrontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './components/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ResourceManagementFrontend';
+
+    constructor(
+    private authService: AuthService,
+    // private errorService: ErrorService
+  ) {}
+
+  ngOnInit() {
+    this.authService.autoAuthUser();
+    // this.errorSub = this.errorService.getErrorListener().subscribe(
+    //   message => this.hasError = message !== null
+    // );
+  }
 }
